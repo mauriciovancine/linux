0dad31954a88cbdb3019ba171371289bfd0e5676
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2018-12-03 00:13:12 +0000

    Update post_install_ubuntu_ecologists.sh

--- install/post_install_ubuntu_ecologists.sh
+++ install/post_install_ubuntu_ecologists.sh
@@ -2,6 +2,12 @@
 
 ## repository
 sudo add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
+sudo add-apt-repository -y "deb https://qgis.org/ubuntu-nightly bionic main"
+
+## public keys
+wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import
+gpg --fingerprint CAEB3DC3BDF7FB45
+gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
 
 ## ppas
 sudo add-apt-repository -y ppa:grass/grass-stable
@@ -10,8 +16,8 @@ sudo apt update -y
 
 ## apps
 sudo apt install -y\
- 
  default-jre\
+ deluge\
  etcher-electron\
  flatpak\
  gnome-software-plugin-flatpak\
@@ -19,23 +25,34 @@ sudo apt install -y\
  grass\
  gimp\
  inkscape\
+ libcairo2-dev\
  libgdal-dev\
+ libjpeg-dev\
+ libgmp3-dev\
+ libgstreamer0.10-0\
+ libgstreamer-plugins-base0.10-0\
  libreoffice\
  libproj-dev\
+ libssl-dev\
+ libudunits2-dev\
  openjdk-11-jre-headless\
+ python-qgis\
+ qgis\
+ qgis-plugin-grass\
  r-base\
  r-base-dev\
  r-cran-curl\
  r-cran-openssl\
  r-cran-xml2\
  rhythmbox\
+ saga\
  sublime-text\
  speedtest-cli\
  spyder3\
 
 
 ## flatpak
-flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # qgis
 flatpak install flathub -y org.qgis.qgis
@@ -47,33 +64,40 @@ flatpak install flathub -y org.gnome.Games
 ## deb
 # wps
 wget http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb
-sudo dpkg -iy wps-office_10.1.0.6757_amd64.deb
+sudo dpkg -i wps-office_10.1.0.6757_amd64.deb
 rm wps-office_10.1.0.6757_amd64.deb
 
 # rstudio
 wget https://download1.rstudio.org/rstudio-1.1.463-amd64.deb
-sudo dpkg -iy rstudio-1.1.463-amd64.deb
+sudo dpkg -i rstudio-1.1.463-amd64.deb
 rm rstudio-1.1.463-amd64.deb
 
 # teamviwer
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo dpkg -iy teamviewer_14.0.14470_amd64.deb
-rm teamviewer_14.0.14470_amd64.deb
+sudo dpkg -i teamviewer_amd64.deb
+rm teamviewer_amd64.deb
 
 # megasync
 wget https://mega.nz/linux/MEGAsync/xUbuntu_$(lsb_release -rs)/amd64/megasync-xUbuntu_$(lsb_release -rs)_amd64.deb -O megasync.deb
-sudo dpkg -iy megasync.deb
-sudo dpkg -iy nautilus-megasync.deb
+sudo dpkg -i megasync.deb
 rm megasync.deb
-rm nautilus-megasync.deb
 
 # skype
 wget https://go.skype.com/skypeforlinux-64.deb
-sudo dpkg -iy skypeforlinux-64.deb
+sudo dpkg -i skypeforlinux-64.deb
 rm skypeforlinux-64.deb
+
+# stacer
+wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.9/stacer_1.0.9_amd64.deb -O stacer.deb
+sudo dpkg -i stacer.deb
+rm stacer.deb
 
 ## java
 sudo R CMD javareconf -y JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/bin/jar
 
-# cleanup apt
+## fix broken
+sudo apt -yf install
+sudo apt -y --fix-broken install
+
+## cleanup apt
 sudo apt autoremove -y --purge
