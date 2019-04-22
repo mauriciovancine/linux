8a20c8c1758a8c78262b93df037d81e1ecd17c09
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2019-04-22 11:28:03 +0000

    Update script_post_install_ubuntu.sh

--- post_install/script_post_install_ubuntu.sh
+++ post_install/script_post_install_ubuntu.sh
@@ -96,10 +96,10 @@ sudo apt install ttf-mscorefonts-installer
 # libreoffice
 wget https://download.documentfoundation.org/libreoffice/stable/6.2.3/deb/x86_64/LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
 tar -xzvf LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
-cd ~/LibreOffice_6.2.2.3_Linux_x86-64_deb/DEBS
+cd ~/LibreOffice_6.2.3.2_Linux_x86-64_deb/DEBS
 sudo dpkg -i *.deb 
 cd ..; cd ..
-rm -r LibreOffice_6.2.2.3_Linux_x86-64_deb
+rm -r LibreOffice_6.2.3.2_Linux_x86-64_deb
 rm LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
 
 # mendeley
