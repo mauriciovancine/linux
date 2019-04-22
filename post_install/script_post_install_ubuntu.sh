b8788866e2fb2acbecd9cb9366115003164e32b1
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2019-04-22 11:19:13 +0000

    Update script_post_install_ubuntu.sh

--- post_install/script_post_install_ubuntu.sh
+++ post_install/script_post_install_ubuntu.sh
@@ -7,109 +7,6 @@ sudo apt upgrade -y
 ## repositories
 sudo add-apt-repository ppa:papirus/papirus
 sudo apt-add-repository ppa:tista/adapta
-sudo add-apt-repository "deb https://qgis.org/ubuntu $(lsb_release -cs) main"
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/"
-sudo add-apt-repository ppa:marutter/c2d4u3.5
-
-## public keys
-wget -O - https://qgis.org/downloads/qgis-2017.gpg.key | gpg --import
-gpg --fingerprint CAEB3DC3BDF7FB45
-gpg --export --armor CAEB3DC3BDF7FB45 | sudo apt-key add -
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-gpg --keyserver keyserver.ubuntu.com --recv-key E298A3A825C0D65DFD57CBB651716619E084DAB9
-gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
-
-## ppas
-sudo apt update -y
-
-## apps
-sudo apt install -y\
- adapta-gtk-theme\
- adapta-backgrounds\
- default-jre\
- etcher-electron\
- gnome-tweak-tool\
- gnome-shell-extensions\
- gparted\
- grass\
- gimp\
- inkscape\
- libcairo2-dev\
- libgdal-dev\
- libjpeg-dev\
- libgmp3-dev\
- libreoffice\
- libproj-dev\
- libssl-dev\
- libudunits2-dev\
- openjdk-11-jre-headless\
- papirus-icon-theme\
- python-qgis\
- qgis\
- qgis-plugin-grass\
- r-base\
- r-base-dev\
- r-cran-curl\
- r-cran-openssl\
- r-cran-rjava\
- r-cran-xml2\
- rhythmbox\
- saga\
- sublime-text\
- speedtest-cli\
- spyder3
-
-## deb
-# wps
-#wget http://kdl.cc.ksosoft.com/wps-community/download/6757/wps-office_10.1.0.6757_amd64.deb
-#sudo dpkg -i wps-office_10.1.0.6757_amd64.deb
-#rm wps-office_10.1.0.6757_amd64.deb
-
-# freeoffice
-#wget https://www.softmaker.net/down/softmaker-freeoffice-2018_942-01_amd64.deb
-#sudo gdebi softmaker-freeoffice-2018_942-01_amd64.deb
-#rm softmaker-freeoffice-2018_942-01_amd64.deb
-
-# teamviwer
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo dpkg -i teamviewer_amd64.deb
-rm teamviewer_amd64.deb
-
-# megasync
-wget https://mega.nz/linux/MEGAsync/xUbuntu_$(lsb_release -rs)/amd64/megasync-xUbuntu_$(lsb_release -rs)_amd64.deb -O megasync.deb
-sudo dpkg -i megasync.deb
-rm megasync.deb
-
-# rstudio
-wget http://ftp.ca.debian.org/debian/pool/main/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-sudo dpkg -i libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-sudo apt-mark hold libgstreamer-plugins-base0.10-0
-rm libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-
-wget http://ftp.ca.debian.org/debian/pool/main/g/gst-plugins-base0.10/libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-sudo dpkg -i libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-sudo apt-mark hold libgstreamer0.10
-rm libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-
-wget https://s3.amazonaws.com/rstudio-dailybuilds/rstudio-1.1.463-amd64.deb
-sudo gdebi rstudio-1.1.463-amd64.deb
-rm rstudio-1.1.463-amd64.deb
-
-# stacer
-wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.9/stacer_1.0.9_amd64.deb -O stacer.deb
-sudo dpkg -i stacer.deb
-rm stacer.deb
-
-#!/bin/bash
-
-## upgrade 
-sudo apt update -y
-sudo apt upgrade -y
-
-## repositories
-sudo add-apt-repository ppa:papirus/papirus
-sudo apt-add-repository ppa:tista/adapta
 sudo add-apt-repository ppa:marutter/c2d4u3.5
 
 ## repositories to edit sourcelist
@@ -145,11 +42,8 @@ sudo apt update -y
 
 ## apps
 sudo apt install -y\
- brasero\
  default-jre\
- gedit\
  git-all\
- gparted\
  grass\
  gimp\
  inkscape\
@@ -172,9 +66,8 @@ sudo apt install -y\
  r-cran-rjava\
  r-cran-xml2\
  saga\
- stacer\
  sublime-text\
- speedtest-cli\
+ speedtest\
  spyder3\
  texlive-full\
  typora
@@ -201,13 +94,13 @@ rm wps-office-fonts_1.0_all.deb
 sudo apt install ttf-mscorefonts-installer
 
 # libreoffice
-wget https://download.documentfoundation.org/libreoffice/stable/6.2.2/deb/x86_64/LibreOffice_6.2.2_Linux_x86-64_deb.tar.gz
-tar -fvx LibreOffice_6.2.2_Linux_x86-64_deb.tar.gz
-cd ~/LibreOffice_6.2.2.2_Linux_x86-64_deb/DEBS
+wget https://download.documentfoundation.org/libreoffice/stable/6.2.3/deb/x86_64/LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
+tar -fvx LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
+cd ~/LibreOffice_6.2.2.3_Linux_x86-64_deb/DEBS
 sudo dpkg -i *.deb 
 cd ..; cd ..
-rm -r LibreOffice_6.2.2.2_Linux_x86-64_deb
-rm LibreOffice_6.2.2_Linux_x86-64_deb.tar.gz
+rm -r LibreOffice_6.2.2.3_Linux_x86-64_deb
+rm LibreOffice_6.2.3_Linux_x86-64_deb.tar.gz
 
 # mendeley
 wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
@@ -229,22 +122,6 @@ wget https://www.thefanclub.co.za/sites/all/modules/pubdlcnt/pubdlcnt.php?file=h
 sudo dpkg -i overgrive_3.2.3_all.deb
 rm overgrive_3.2.3_all.deb
 
-# megasync
-# wget https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb -O megasync.deb
-# sudo dpkg -i megasync.deb
-# rm megasync.deb
-
-# rstudio
-# wget http://ftp.ca.debian.org/debian/pool/main/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-# sudo dpkg -i libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-# sudo apt-mark hold libgstreamer-plugins-base0.10-0
-# rm libgstreamer0.10-0_0.10.36-1.5_amd64.deb
-
-# wget http://ftp.ca.debian.org/debian/pool/main/g/gst-plugins-base0.10/libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-# sudo dpkg -i libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-# sudo apt-mark hold libgstreamer0.10
-# rm libgstreamer-plugins-base0.10-0_0.10.36-2_amd64.deb
-
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.1335-amd64.deb
 sudo apt install dpkg-sig
 dpkg-sig --verify rstudio-1.2.1335-amd64.deb
@@ -256,26 +133,10 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 rm gitkraken-amd64.deb
 
-# sublime merge
-wget https://download.sublimetext.com/sublime-merge_build-1107_amd64.deb
-sudo dpkg -i sublime-merge_build-1107_amd64.deb
-rm sublime-merge_build-1107_amd64.deb
-
 # stacer
 wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.9/stacer_1.0.9_amd64.deb
 sudo dpkg -i stacer_1.0.9_amd64.deb
 rm stacer_1.0.9_amd64.deb
-
-## fix broken
-sudo apt clean && sudo apt update
-sudo dpkg --configure -a
-sudo apt install -f
-sudo apt install -y --fix-broken 
-
-## cleanup apt
-sudo apt autoremove -y
-sudo apt autoclean -y
-
 
 ## fix broken
 sudo apt clean && sudo apt update
