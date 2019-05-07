19d42d00269e70abef585c6446304562fed119a0
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2019-05-07 17:16:36 +0000

    Update googledrive_mint.sh

--- sync/googledrive_mint.sh
+++ sync/googledrive_mint.sh
@@ -8,9 +8,25 @@
 # https://olivermarshall.net/how-to-upload-a-file-to-google-drive-from-the-command-line/
 
 # install -------------------------------------------------------
-
 wget https://drive.google.com/uc?id=1Ej8VgsW5RgK66Btb9p74tSdHMH3p4UNb&export=download
 mv uc?id=1Ej8VgsW5RgK66Btb9p74tSdHMH3p4UNb gdrive-linux-x64
 chmod +x gdrive-linux-x64
 sudo install gdrive /usr/local/bin/gdrive-linux-x64
 
+# use -----------------------------------------------------------
+gdrive
+
+# copy the link from terminal to browser
+# make login
+# copy the link from browser to terminal
+
+# testing your configuration ------------------------------------
+gdrive changes
+
+# show your configuration ---------------------------------------
+gdrive 
+
+# performing a selective directory sync -------------------------
+gdrive 
+
+# end -----------------------------------------------------------
