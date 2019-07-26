a1c042a669cccdde3ecb7b01d3f810d4a8dc4dc4
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2019-07-26 14:39:43 +0000

    Update post_installl_linux_mint.sh

--- linux_mint/post_installl_linux_mint.sh
+++ linux_mint/post_installl_linux_mint.sh
@@ -47,6 +47,12 @@ sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt update
 sudo apt install -y\
   qgis\
   qgis-plugin-grass
+  
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install -y sublime-text
 
 # r
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
