5a72e5afdfc95c8339468ad0678ab7ef843e5050
Maurício Humberto Vancine <mauricio.vancine@gmail.com>
2019-07-30 21:52:54 +0000

    Update post_installl_linux_mint.sh

--- linux_mint/post_installl_linux_mint.sh
+++ linux_mint/post_installl_linux_mint.sh
@@ -85,6 +85,15 @@ sudo apt install -y\
 # spyder
 sudo apt install -y spyder3
 
+# trello
+wget https://github.com/danielchatfield/trello-desktop/releases/download/v0.1.9/Trello-linux-0.1.9.zip -O trello.zip
+sudo mkdir /opt/trello
+sudo unzip trello.zip -d /opt/trello/
+sudo ln -sf /opt/trello/Trello /usr/bin/trello
+echo -e '[Desktop Entry]\n Version=1.0\n Name=trello\n Exec=/opt/trello/Trello\n Icon=/opt/trello/resources/app/static/Icon.png\n Type=Application\n Categories=Application' | sudo tee /usr/share/applications/trello.desktop
+sudo chmod +x /usr/share/applications/trello.desktop
+cp /usr/share/applications/trello.desktop ~/Desktop
+
 # fonts
 sudo apt install -y ubuntu-restricted-extras
 
