36abff160b2eb925f47d175220c71e7c5bb4ce40
Maurício Vancine <mauricio.vancine@gmail.com>
2019-10-31 01:39:39 +0000

    Update post_install_manjaro.sh

--- post_install/post_install_manjaro.sh
+++ post_install/post_install_manjaro.sh
@@ -1,36 +1,35 @@
 ### script post install manjaro ###
 
 # mauricio vancine
-# 30-04-2019
+# 30-10-2019
 
 # update repository and system
 sudo pacman -Syyu
 
+
+# packages
+# install rhythmbox
+sudo pacman -Sy rhythmbox
+
 # install r
 sudo pacman -Sy r
-pamac build udunits # sf
 
 # install qgis
 sudo pacman -Sy qgis
 
-# install rhythmbox
-sudo pacman -Sy rhythmbox
 
 # aur
 # install google chrome
 pamac build -Sy google-chrome
 
+# install simplenote
+pamac build -Sy simplenote-electron-bin
+
 # install rstudio
 pamac build -Sy rstudio-desktop
 
-# install grass
-pamac build -Sy grass
-
-# install inkscape
-pamac build -Sy inkscape
-
-# install simplenote
-pamac build -Sy simplenote-electron-bin
+# r packages
+pamac build udunits # sf
 
 # install sublime text
 pamac build -Sy sublime-text-dev
@@ -38,7 +37,13 @@ pamac build -Sy sublime-text-dev
 # install gitkraken
 pamac build -Sy gitkraken
 
-# install sublime wps-office
+# install grass
+pamac build -Sy grass
+
+# install inkscape
+pamac build -Sy inkscape
+
+# install wps-office
 pamac build -Sy wps-office ttf-wps-fonts wps-office-extension-portuguese-brazilian-dictionary
 
 # java
