41676e485399cf6681db6bf9ce199b114365d578
Maurício Vancine <mauricio.vancine@gmail.com>
2019-11-04 11:19:32 +0000

    Update post_install_manjaro.sh

--- post_install/post_install_manjaro.sh
+++ post_install/post_install_manjaro.sh
@@ -34,6 +34,54 @@ sudo pacman -Sy speedtest-cli
 
 # aur
 # r packages
+pamac install udunits # units
+
+# install google chrome
+pamac install google-chrome
+
+# install simplenote
+pamac install simplenote-electron-bin
+
+# install rstudio
+pamac install rstudio-desktop
+
+# r packages
+pamac install udunits # sf
+
+# install sublime text
+pamac install sublime-text-dev
+
+# install gitkraken
+pamac install gitkraken
+
+# install qgis
+pamac install qgis-ltr
+
+# python for qgis
+sudo pacman -Sy python-pyaml python-psycopg2 python-owslib python-pygments
+pamac install python-pyjade
+
+# install grass
+pamac install grass
+
+# install wps-office
+pamac install wps-office ttf-wps-fonts wps-office-extension-portuguese-brazilian-dictionary
+
+# install slack-desktop
+pamac install slack-desktop
+
+# install whatsapp
+pamac install whatsapp-nativefier
+
+# install etcher
+pamac install balena-etcher
+
+
+
+# ----------------------------------------------------------------------
+
+# aur
+# r packages
 pamac build udunits # units
 
 # install google chrome
