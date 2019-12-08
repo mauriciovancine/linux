9a401e0e24a3c77c372b306d343014c960e46a6b
Maurício Vancine <mauricio.vancine@gmail.com>
2019-12-08 20:51:51 -0300

    Update post_install_linux_mint.sh

--- post_install/post_install_linux_mint.sh
+++ post_install/post_install_linux_mint.sh
@@ -159,7 +159,7 @@ sudo snap install evernote-web-client
 ## dpkgs --------------------------------------------------------------------------------------
 # google chrome
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
 # libreoffice
