0161cccd99ade0f9056dbc63d0938b12d0120200
Maurício Vancine <mauricio.vancine@gmail.com>
2020-02-27 10:56:14 -0300

    Update post_install_linux_mint.sh

--- post_install/post_install_linux_mint.sh
+++ post_install/post_install_linux_mint.sh
@@ -188,14 +188,14 @@ rm Simplenote-linux-1.14.0-amd64.deb
 # libreoffice
 sudo apt remove -y libreoffice*
 
-wget -c https://download.documentfoundation.org/libreoffice/stable/6.4.0/deb/x86_64/LibreOffice_6.4.0_Linux_x86-64_deb.tar.gz
-tar xvzf LibreOffice_6.4.0_Linux_x86-64_deb.tar.gz
-cd ~/LibreOffice_6.4.0.3_Linux_x86-64_deb/DEBS
+wget -c https://download.documentfoundation.org/libreoffice/stable/6.4.1/deb/x86_64/LibreOffice_6.4.1_Linux_x86-64_deb.tar.gz
+tar xvzf LibreOffice_6.4.1_Linux_x86-64_deb.tar.gz
+cd ~/LibreOffice_6.4.1.2_Linux_x86-64_deb/DEBS
 sudo dpkg -i *.deb
 sudo apt install -fy
 cd ..; cd ..
-rm -r LibreOffice_6.4.0.3_Linux_x86-64_deb
-rm LibreOffice_6.4.0_Linux_x86-64_deb.tar.gz
+rm -r LibreOffice_6.4.1.2_Linux_x86-64_deb
+rm LibreOffice_6.4.1_Linux_x86-64_deb.tar.gz
 
 # warsal - modulo seguranca bb
 wget -c https://cloud.gastecnologia.com.br/bb/downloads/ws/warsaw_setup64.deb
