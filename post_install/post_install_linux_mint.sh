6cc5a57ed98b1b21052d591b6457c478ca373a1e
Maurício Vancine <mauricio.vancine@gmail.com>
2020-03-02 16:05:46 -0300

    Update post_install_linux_mint.sh

--- post_install/post_install_linux_mint.sh
+++ post_install/post_install_linux_mint.sh
@@ -69,7 +69,7 @@ sudo su -
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 apt update
-apt install anydesk
+apt install anydesk -y
 exit
 
 # java
