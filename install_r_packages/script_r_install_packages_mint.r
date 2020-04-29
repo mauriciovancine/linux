b82599c5fb175a06ee4b0e90c6dec6a88542b278
Maurício Vancine <mauricio.vancine@gmail.com>
2020-04-29 15:42:48 -0300

    Update script_r_install_packages_mint.r

--- install_r_packages/script_r_install_packages_mint.r
+++ install_r_packages/script_r_install_packages_mint.r
@@ -1,61 +1,62 @@
-### install r packages - linux mint ###
+### install r packages - ubuntu 20.04 ###
 
 # github ------------------------------------------------------------------
-if(!require("devtools")) install.packages("devtools")
+if(!require(devtools)) install.packages("devtools")
 
 # internet ----------------------------------------------------------------
-if(!require("Rcpp")) install.packages("Rcpp")
-if(!require("httpuv")) install.packages("httpuv")
+if(!require(Rcpp)) install.packages("Rcpp")
+if(!require(httpuv)) install.packages("httpuv")
 
 # tidyverse ---------------------------------------------------------------
-if(!require("broom")) install.packages("broom")
-if(!require("cli")) install.packages("cli")         
-if(!require("crayon")) install.packages("crayon")      
-if(!require("dplyr")) install.packages("dplyr")       
-if(!require("dbplyr")) install.packages("dbplyr")      
-if(!require("forcats")) install.packages("forcats")    
-if(!require("ggplot2")) install.packages("ggplot2")     
-if(!require("haven")) install.packages("haven")
-if(!require("hms")) install.packages("hms")
-if(!require("httr")) install.packages("httr")
-if(!require("jsonlite")) install.packages("jsonlite")
-if(!require("lubridate")) install.packages("lubridate")  
-if(!require("magrittr")) install.packages("magrittr")
-if(!require("modelr")) install.packages("modelr")
-if(!require("purrr")) install.packages("purrr")
-if(!require("readODS")) install.packages("readODS")
-if(!require("readr")) install.packages("readr")
-if(!require("readxl")) install.packages("readxl")
-if(!require("reprex")) install.packages("reprex")
-if(!require("rlang")) install.packages("rlang")
-if(!require("rstudioapi")) install.packages("rstudioapi")
-if(!require("rvest")) install.packages("rvest")       
-if(!require("stringr")) install.packages("stringr")
-if(!require("tibble")) install.packages("tibble")
-if(!require("tidyr")) install.packages("tidyr")      
-if(!require("xml2")) install.packages("xml2")
-if(!require("tidyverse")) install.packages("tidyverse") # sudo apt install libssl-dev
-if(!require("writexl")) install.packages("writexl")
+if(!require(broom)) install.packages("broom")
+if(!require(cli)) install.packages("cli")         
+if(!require(crayon)) install.packages("crayon")      
+if(!require(dplyr)) install.packages("dplyr")       
+if(!require(dbplyr)) install.packages("dbplyr")      
+if(!require(forcats)) install.packages("forcats")    
+if(!require(ggplot2)) install.packages("ggplot2")     
+if(!require(haven)) install.packages("haven")
+if(!require(hms)) install.packages("hms")
+if(!require(httr)) install.packages("httr")
+if(!require(jsonlite)) install.packages("jsonlite")
+if(!require(lubridate)) install.packages("lubridate")  
+if(!require(magrittr)) install.packages("magrittr")
+if(!require(modelr)) install.packages("modelr")
+if(!require(purrr)) install.packages("purrr")
+if(!require(readODS)) install.packages("readODS")
+if(!require(readr)) install.packages("readr")
+if(!require(readxl)) install.packages("readxl")
+if(!require(reprex)) install.packages("reprex")
+if(!require(rlang)) install.packages("rlang")
+if(!require(rstudioapi)) install.packages("rstudioapi")
+if(!require(rvest)) install.packages("rvest")       
+if(!require(stringr)) install.packages("stringr")
+if(!require(tibble)) install.packages("tibble")
+if(!require(tidyr)) install.packages("tidyr")      
+if(!require(xml2)) install.packages("xml2")
+if(!require(tidyverse)) install.packages("tidyverse") # sudo apt install libssl-dev
+if(!require(writexl)) install.packages("writexl")
 
 tidyverse_packages(include_self = TRUE)
 
 # ecological analysis -----------------------------------------------------
-if(!require("iNEXT")) install.packages("iNEXT")
-if(!require("ade4")) install.packages("ade4")
-if(!require("adespatial")) install.packages("adespatial") # sudo apt install -y libudunits2-dev
-if(!require("betapart")) install.packages("betapart") # sudo apt install libgmp3-dev
-if(!require("factoextra")) install.packages("factoextra")
-if(!require("FactoMineR")) install.packages("FactoMineR")
-if(!require("spdep")) install.packages("spdep")
-if(!require("vegan")) install.packages("vegan")
-if(!require("ggvegan")) devtools::install_github("gavinsimpson/ggvegan")
-if(!require("ggord")) devtools::install_github("fawda123/ggord")
+if(!require(iNEXT)) install.packages("iNEXT")
+if(!require(ade4)) install.packages("ade4")
+if(!require(adespatial)) install.packages("adespatial") # sudo apt install -y libudunits2-dev
+if(!require(betapart)) install.packages("betapart") # sudo apt install libgmp3-dev
+if(!require(factoextra)) install.packages("factoextra")
+if(!require(FactoMineR)) install.packages("FactoMineR")
+if(!require(spdep)) install.packages("spdep")
+if(!require(vegan)) install.packages("vegan")
+if(!require(ggvegan)) devtools::install_github("gavinsimpson/ggvegan")
+if(!require(ggord)) devtools::install_github("fawda123/ggord")
 
 # plot --------------------------------------------------------------------
-if(!require("GGally")) install.packages("GGally")
-if(!require("ggpubr")) install.packages("ggpubr")
-if(!require("cowplot")) install.packages("cowplot")
-if(!require("gridExtra")) install.packages("gridExtra")
+if(!require(GGally)) install.packages("GGally")
+if(!require(ggpubr)) install.packages("ggpubr")
+if(!require(cowplot)) install.packages("cowplot")
+if(!require(gridExtra)) install.packages("gridExtra")
+if(!require(patchwork)) install.packages("patchwork")
 
 # colors ------------------------------------------------------------------
 # https://github.com/EmilHvitfeldt/r-color-palettes
@@ -93,96 +94,97 @@ if(!require(dutchmasters)) devtools::install_github("EdwinTh/dutchmasters")
 if(!require(awtools)) devtools::install_github("awhstin/awtools")
 
 # data table --------------------------------------------------------------
-if(!require("data.table")) install.packages("data.table")
+if(!require(data.table)) install.packages("data.table")
 
 # spatial analysis --------------------------------------------------------
-if(!require("sp")) install.packages("sp")
-if(!require("raster")) install.packages("raster")
-if(!require("rgdal")) install.packages("rgdal") # sudo apt install libgdal-dev libproj-dev
-if(!require("rgeos")) install.packages("rgeos")
-if(!require("units")) install.packages("units")
-if(!require("udunits2")) install.packages("udunits2") # sudo apt install libudunits2-dev
-if(!require("sf")) install.packages("sf")
-if(!require("fasterize")) install.packages("fasterize")
-if(!require("ggspatial")) install.packages("ggspatial")
+if(!require(sp)) install.packages("sp")
+if(!require(raster)) install.packages("raster")
+if(!require(rgdal)) install.packages("rgdal") # sudo apt install libgdal-dev libproj-dev
+if(!require(rgeos)) install.packages("rgeos")
+if(!require(units)) install.packages("units")
+if(!require(udunits2)) install.packages("udunits2") # sudo apt install libudunits2-dev
+if(!require(sf)) install.packages("sf")
+if(!require(fasterize)) install.packages("fasterize")
+if(!require(ggspatial)) install.packages("ggspatial")
 
 # maps --------------------------------------------------------------------
-if(!require("tmap")) install.packages("tmap")
-if(!require("tmaptools")) install.packages("tmaptools")
-if(!require("maptools")) install.packages("maptools")
-if(!require("mapview")) install.packages("mapview")
-if(!require("leaflet")) install.packages("leaflet")
-if(!require("cartography")) install.packages("cartography")
-if(!require("rasterVis")) install.packages("rasterVis")
+if(!require(lwgeom)) devtools::install_github("r-spatial/lwgeom")
+if(!require(tmap)) install.packages("tmap")
+if(!require(tmaptools)) install.packages("tmaptools")
+if(!require(maptools)) install.packages("maptools")
+if(!require(mapview)) install.packages("mapview")
+if(!require(leaflet)) install.packages("leaflet")
+if(!require(cartography)) install.packages("cartography")
+if(!require(rasterVis)) install.packages("rasterVis")
 
 # landscape metrics -------------------------------------------------------
-if(!require("landscapeverse")) devtools::install_github("r-spatialecology/landscapeverse")
-if(!require("landscapemetrics")) install.packages("landscapemetrics")
-if(!require("NLMR")) install.packages("NLMR")
-if(!require("landscapetools")) install.packages("landscapetools")
+if(!require(landscapeverse)) devtools::install_github("r-spatialecology/landscapeverse")
+if(!require(landscapemetrics)) install.packages("landscapemetrics")
+if(!require(NLMR)) install.packages("NLMR")
+if(!require(landscapetools)) install.packages("landscapetools")
 
 # remote sensing ----------------------------------------------------------
-if(!require("getlandsat")) install.packages("getlandsat")
-if(!require("RStoolbox")) install.packages("RStoolbox")
+if(!require(getlandsat)) install.packages("getlandsat")
+if(!require(RStoolbox)) install.packages("RStoolbox")
 
 # species distribution modeling -----------------------------------------------
 # occ - download
-if(!require("rgbif")) install.packages("rgbif")
-if(!require("spocc")) install.packages("spocc")
-if(!require("BIEN")) install.packages("BIEN")
-if(!require("marinespeed")) install.packages("marinespeed")
-if(!require("robis")) install.packages("robis")
+if(!require(rgbif)) install.packages("rgbif")
+if(!require(spocc)) install.packages("spocc")
+if(!require(BIEN)) install.packages("BIEN")
+if(!require(marinespeed)) install.packages("marinespeed")
+if(!require(robis)) install.packages("robis")
 
 # occ - taxonomy
-if(!require("worrms")) install.packages("worrms")
-if(!require("rfishbase")) install.packages("rfishbase")
-if(!require("taxize")) install.packages("taxize")
+if(!require(worrms)) install.packages("worrms")
+if(!require(rfishbase)) install.packages("rfishbase")
+if(!require(taxize)) install.packages("taxize")
 
 # occ - clear
-if(!require("CoordinateCleaner")) install.packages("CoordinateCleaner")
-if(!require("speciesgeocodeR")) devtools::install_github("azizka/speciesgeocodeR")
-if(!require("sampbias")) devtools::install_github("azizka/sampbias")
+if(!require(CoordinateCleaner)) install.packages("CoordinateCleaner")
+if(!require(speciesgeocodeR)) devtools::install_github("azizka/speciesgeocodeR")
+if(!require(sampbias)) devtools::install_github("azizka/sampbias")
 
 # var - limits
-if(!require("rnaturalearth")) install.packages("rnaturalearth")
-if(!require("rnaturalearthdata")) install.packages("rnaturalearthdata")
-if(!require("rnaturalearthhires")) devtools::install_github("ropensci/rnaturalearthhires")
-if(!require("geobr")) install.packages("geobr")
+if(!require(rnaturalearth)) install.packages("rnaturalearth")
+if(!require(rnaturalearthdata)) install.packages("rnaturalearthdata")
+if(!require(rnaturalearthhires)) devtools::install_github("ropensci/rnaturalearthhires")
+if(!require(geobr)) install.packages("geobr")
 
 # var - download
-if(!require("sdmpredictors")) install.packages("sdmpredictors")
+if(!require(sdmpredictors)) install.packages("sdmpredictors")
 
 # var - selection
-if(!require("usdm")) install.packages("usdm")
-if(!require("psych")) install.packages("psych")
+if(!require(usdm)) install.packages("usdm")
+if(!require(psych)) install.packages("psych")
 
 # algorithms
-if(!require("biomod2")) install.packages("biomod2")
-if(!require("dismo")) install.packages("dismo")
-if(!require("sdm")) install.packages("sdm")
+if(!require(biomod2)) install.packages("biomod2")
+if(!require(dismo)) install.packages("dismo")
+if(!require(sdm)) install.packages("sdm")
 
 # algorithms only
-if(!require("kernlab")) install.packages("kernlab")
-if(!require("e1071")) install.packages("e1071")
-if(!require("CENFA")) install.packages("CENFA")
-if(!require("earth")) install.packages("earth")
-if(!require("ecospat")) install.packages("ecospat")
-if(!require("gam")) install.packages("gam")
-if(!require("gbm")) install.packages("gbm")
-if(!require("mda")) install.packages("mda")
-if(!require("mgcv")) install.packages("mgcv")
-if(!require("ncf")) install.packages("ncf")
-if(!require("nnet")) install.packages("nnet")
-if(!require("randomForest")) install.packages("randomForest")
-if(!require("rpart")) install.packages("rpart")
-if(!require("rJava")) install.packages("rJava")
-if(!require("ENMeval")) install.packages("ENMeval")
-if(!require("maxnet")) install.packages("maxnet")
-if(!require("maxlike")) install.packages("maxlike")
-if(!require("kuenm")) devtools::install_github("marlonecobos/kuenm")
+if(!require(kernlab)) install.packages("kernlab")
+if(!require(e1071)) install.packages("e1071")
+if(!require(CENFA)) install.packages("CENFA")
+if(!require(earth)) install.packages("earth")
+if(!require(ecospat)) install.packages("ecospat")
+if(!require(gam)) install.packages("gam")
+if(!require(gbm)) install.packages("gbm")
+if(!require(mda)) install.packages("mda")
+if(!require(mgcv)) install.packages("mgcv")
+if(!require(ncf)) install.packages("ncf")
+if(!require(nnet)) install.packages("nnet")
+if(!require(randomForest)) install.packages("randomForest")
+if(!require(rpart)) install.packages("rpart")
+if(!require(rJava)) install.packages("rJava")
+if(!require(ENMeval)) install.packages("ENMeval")
+if(!require(maxnet)) install.packages("maxnet")
+if(!require(maxlike)) install.packages("maxlike")
+if(!require(kuenm)) devtools::install_github("marlonecobos/kuenm")
 
 # ensemble
-if(!require("eSDM")) install.packages("eSDM")
+if(!require(eSDM)) install.packages("eSDM")
 
 # hierarchical bayesian
 # hsdm
@@ -194,53 +196,52 @@ if(!require("eSDM")) install.packages("eSDM")
 # make
 # sudo make install
 
-if(!require("hSDM")) install.packages("hSDM")
+if(!require(hSDM)) install.packages("hSDM")
 
 # joint
-if(!require("jSDM")) install.packages("jSDM") # sudo apt install -y libgsl-dev
+if(!require(jSDM)) install.packages("jSDM") # sudo apt install -y libgsl-dev
 
 # stacked species distribution models
-if(!require("SSDM")) install.packages("SSDM")
+if(!require(SSDM)) install.packages("SSDM")
 
 # virtual species
-if(!require("sdmvspecies")) install.packages("sdmvspecies")
-if(!require("virtualspecies")) install.packages("virtualspecies")
+if(!require(sdmvspecies)) install.packages("sdmvspecies")
+if(!require(virtualspecies)) install.packages("virtualspecies")
 
 # euclidean distance
 # https://rpubs.com/Bruno_Vilela/279257
 
 # markdown ----------------------------------------------------------------
-if(!require("rmarkdown")) install.packages("rmarkdown")
-if(!require("knitr")) install.packages("knitr")
-if(!require("tinytex")) install.packages("tinytex"); tinytex::install_tinytex()
-if(!require("blogdown")) install.packages("blogdown")
-blogdown::install_hugo()
-if(!require("pagedown")) install.packages("pagedown")
-if(!require("bookdown")) install.packages("bookdown")
-if(!require("posterdown")) devtools::install_github("brentthorne/posterdown")
-if(!require("xaringan")) install.packages("xaringan")
-if(!require("xaringanthemer")) devtools::install_github("gadenbuie/xaringanthemer")
-if(!require("icon")) devtools::install_github("ropenscilabs/icon")
+if(!require(rmarkdown)) install.packages("rmarkdown")
+if(!require(knitr)) install.packages("knitr")
+if(!require(tinytex)) install.packages("tinytex")
+if(!require(blogdown)) install.packages("blogdown")
+if(!require(pagedown)) install.packages("pagedown")
+if(!require(bookdown)) install.packages("bookdown")
+if(!require(posterdown)) devtools::install_github("brentthorne/posterdown")
+if(!require(xaringan)) install.packages("xaringan")
+if(!require(xaringanthemer)) devtools::install_github("gadenbuie/xaringanthemer")
+if(!require(icon)) devtools::install_github("ropenscilabs/icon")
 
 # email -------------------------------------------------------------------
-if(!require("gmailr")) install.packages("gmailr")
+if(!require(gmailr)) install.packages("gmailr")
 
 # googledrive -------------------------------------------------------------
-if(!require("googledrive")) install.packages("googledrive")
+if(!require(googledrive)) install.packages("googledrive")
 
 # beep --------------------------------------------------------------------
-if(!require("beepr")) install.packages("beepr")
+if(!require(beepr)) install.packages("beepr")
 
 # hexagon -----------------------------------------------------------------
-if(!require("hexSticker")) install.packages("hexSticker")
+if(!require(hexSticker)) install.packages("hexSticker")
 
 # python ------------------------------------------------------------------
-if(!require("reticulate")) install.packages("reticulate")
+if(!require(reticulate)) install.packages("reticulate")
 
 # shiny -------------------------------------------------------------------
-if(!require("shiny")) install.packages("shiny")
-if(!require("shinyjs")) install.packages("shinyjs")
-if(!require("shinydashboard")) install.packages("shinydashboard")
-if(!require("DT")) install.packages("DT")
+if(!require(shiny)) install.packages("shiny")
+if(!require(shinyjs)) install.packages("shinyjs")
+if(!require(shinydashboard)) install.packages("shinydashboard")
+if(!require(DT)) install.packages("DT")
 
 # end ---------------------------------------------------------------------
