9bef8921f419ca2061bc022d50b26c70845092cc
Maurício Vancine <mauricio.vancine@gmail.com>
2020-05-01 17:44:57 -0300

    Update post_install_ubuntu.sh

--- post_install/post_install_ubuntu.sh
+++ post_install/post_install_ubuntu.sh
@@ -11,7 +11,9 @@ sudo apt install -y gnome-shell-extensions &&
 sudo apt install -y chrome-gnome-shell &&
 sudo apt install -y gnome-shell-extension-weather &&
 sudo apt install -y x11-utils &&
-sudo apt install -y gir1.2-gtop-2.0 lm-sensors gnome-shell-extension-prefs
+sudo apt install -y gir1.2-gtop-2.0 lm-sensors gnome-shell-extension-prefs &&
+mkdir -p ~/.local/share/gnome-shell/extensions &&
+git clone https://github.com/corecoding/Vitals.git ~/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com
 # after: press Alt + F2 and enter r in the box
 
 # git
@@ -20,10 +22,10 @@ sudo apt update &&
 sudo apt install -y git
 
 # dash-to-dock
-git clone https://github.com/micheleg/dash-to-dock.git
-cd dash-to-dock
-make
-make install
+# git clone https://github.com/micheleg/dash-to-dock.git
+# cd dash-to-dock
+# make
+# make install
 
 # theme
 sudo add-apt-repository ppa:daniruiz/flat-remix &&
@@ -34,13 +36,19 @@ sudo apt -y install flat-remix-gtk &&
 wget -c https://launchpadlibrarian.net/468844787/paper-icon-theme_1.5.728-202003121505~daily~ubuntu18.04.1_all.deb &&
 sudo dpkg -i paper*.deb &&
 sudo apt install -f &&
-rm https://launchpadlibrarian.net/468844787/paper-icon-theme_1.5.728-202003121505~daily~ubuntu18.04.1_all.deb
+rm paper-icon-theme_1.5.728-202003121505~daily~ubuntu18.04.1_all.deb
 
 # shell
 sudo apt-get install gtk2-engines-murrine gtk2-engines-pixbuf &&
 git clone https://github.com/vinceliuice/Matcha-gtk-theme.git &&
 cd Matcha-gtk-theme &&
 ./install.sh
+
+# wallpapers
+sudo apt-get install -y ubuntu-wallpapers*
+
+# chromium
+sudo apt install -y chromium-browser
 
 # gparted
 sudo apt install -y gparted
@@ -52,13 +60,13 @@ sudo apt install -y gedit
 sudo apt install -y screenfetch
 
 # speedtest
-sudo apt install -y speedtest-cli &&
+sudo apt install -y speedtest-cli
 
 # htop
-sudo apt install -y htop &&
+sudo apt install -y htop
 
 # inkscape
-sudo apt install -y inkscape &&
+sudo apt install -y inkscape
 
 # flameshot
 sudo apt install -y flameshot
@@ -66,51 +74,70 @@ sudo apt install -y flameshot
 # gdal
 sudo apt install -y gdal-bin
 
-# grass
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3 &&
-echo -e "\ndeb https://qgis.org/ubuntu focal main" | sudo tee -a /etc/apt/sources.list &&
-sudo apt update
-sudo apt install -y grass grass-gui
+# fonts
+sudo apt install -y ubuntu-restricted-extras
+
+# typora
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE &&
+echo -e "\ndeb https://typora.io/linux ./" | sudo tee -a /etc/apt/sources.list &&
+sudo apt update &&
+sudo apt install -y typora
+
+# anydesk
+sudo su - &&
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add - &&
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list &&
+apt update &&
+apt install anydesk -y &&
+exit
 
 # qgis
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3 &&
 echo -e "\ndeb https://qgis.org/ubuntu focal main" | sudo tee -a /etc/apt/sources.list &&
 sudo apt update &&
-sudo apt install -y qgis qgis-plugin-grass saga &&
-  
+sudo apt install -y qgis qgis-plugin-grass saga
+
+# grass
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3 &&
+echo -e "\ndeb https://qgis.org/ubuntu focal main" | sudo tee -a /etc/apt/sources.list &&
+sudo apt update &&
+sudo apt install -y grass grass-gui
+
 # sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
+sudo apt update &&
 sudo apt install -y sublime-text
 
 # r
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 &&
 echo -e "\ndeb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" | sudo tee -a /etc/apt/sources.list &&
-sudo apt update
-sudo apt install -y r-base-core # r
-sudo apt install -y r-base-dev # devtools
-sudo apt install -y libssl-dev # tidyverse
-sudo apt install -y libxml2-dev # tidyverse
-sudo apt install -y libcurl4-openssl-dev # tidyverse
-sudo apt install -y libgdal-dev # gdal
-sudo apt install -y libproj-dev # gdal
-sudo apt install -y libudunits2-dev # 
-sudo apt install -y libcairo2-dev # mapview
-sudo apt install -y libgmp3-dev # betapart
-sudo apt install -y libgtk2.0-dev # cairo
-sudo apt install -y xvfb # cairo 
-sudo apt install -y xauth # cairo 
-sudo apt install -y xfonts-base # cairo 
-sudo apt install -y libxt-dev # cairo
+sudo apt update &&
+sudo apt install -y r-base-core && # r
+sudo apt install -y r-base-dev && # devtools
+sudo apt install -y libxml2-dev && # devtools
+sudo apt install -y libssl-dev && # devtools
+sudo apt install -y libcurl4-openssl-dev && # devtools
+sudo apt install -y libgdal-dev && # gdal
+sudo apt install -y libproj-dev && # gdal
+sudo apt install -y libudunits2-dev && # units
+sudo apt install -y libcairo2-dev && # mapview
+sudo apt install -y libv8-dev && # rmapshaper
+sudo apt install -y libprotobuf-dev && # rmapshaper 
+sudo apt install -y libjq-dev && # rmapshaper 
+sudo apt install -y protobuf-compiler && # rmapshaper 
+sudo apt install -y libgmp3-dev && # betapart
+sudo apt install -y libgtk2.0-dev && # cairo
+sudo apt install -y xvfb && # cairo 
+sudo apt install -y xauth && # cairo 
+sudo apt install -y xfonts-base && # cairo 
+sudo apt install -y libxt-dev && # cairo
+sudo apt install -y libgsl-dev && # gsl
+sudo apt install -y libmagick++-dev # magick
 
-# stacer
-sudo add-apt-repository ppa:oguzhaninan/stacer
-sudo apt update
-sudo apt install stacer
-
-# fonts
-sudo apt install -y ubuntu-restricted-extras
+# java
+sudo apt install -y default-jre default-jdk &&
+sudo R CMD javareconf
 
 ## flatpak ------------------------------------------------------------------------------------
 # flatpak
@@ -120,9 +147,6 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # spotify
 flatpak install -y flathub com.spotify.Client
-
-# sublime
-flatpak install -y flathub com.sublimetext.three
 
 # skype
 flatpak install -y flathub com.skype.Client
@@ -137,33 +161,27 @@ flatpak install -y flathub org.telegram.desktop
 flatpak install -y flathub com.elsevier.MendeleyDesktop
 
 ## dpkgs --------------------------------------------------------------------------------------
-# chrome
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-sudo apt install -fy &&
-rm google-chrome-stable_current_amd64.deb
-
 # rstudio
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.5001-amd64.deb
-sudo dpkg -i rstudio-1.2.5001-amd64.deb
-sudo apt install -fy
-rm rstudio-1.2.5001-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.5042-amd64.deb &&
+sudo dpkg -i rstudio-1.2.5042-amd64.deb &&
+sudo apt install -fy && 
+rm rstudio-1.2.5042-amd64.deb
 
 # google earth
-wget http://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
-sudo dpkg -i google-earth-pro-stable_current_amd64.deb
-sudo apt install -fy
+wget http://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb &&
+sudo dpkg -i google-earth-pro-stable_current_amd64.deb &&
+sudo apt install -fy &&
 rm google-earth-pro-stable_current_amd64.deb
 
 ## fix broken
-sudo apt clean && sudo apt update
-sudo dpkg --configure -a
-sudo apt install -f
+sudo apt clean && sudo apt update &&
+sudo dpkg --configure -a &&
+sudo apt install -f &&
 sudo apt install -y --fix-broken 
 
 ## cleanup apt
-sudo apt update
-sudo apt autoremove -y
+sudo apt update &&
+sudo apt autoremove -y &&
 sudo apt autoclean -y
 
 # end ----------------------------------------------------------------------
