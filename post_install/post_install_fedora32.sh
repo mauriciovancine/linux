bd4ae9bc995b1a2c58cfa75435ff2b772a0f6434
Maurício Vancine <mauricio.vancine@gmail.com>
2020-09-26 12:33:15 -0300

    Update post_install_fedora32.sh

--- post_install/post_install_fedora32.sh
+++ post_install/post_install_fedora32.sh
@@ -14,6 +14,14 @@ sudo dnf install paper-icon-theme
 # install r
 sudo dnf install R
 
+# inkskape
+sudo dnf install -y inkscape
+
+# sublime
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+sudo dnf install -y sublime-text
+
 # rstudio
 wget -c https://download1.rstudio.org/desktop/centos8/x86_64/rstudio-1.3.1093-x86_64.rpm &&
 sudo rpm -i rstudio-1.3.1093-x86_64.rpm
