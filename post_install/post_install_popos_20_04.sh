de43b4dec44f293aa69b916db90c49b829cef43c
mauriciovancine <mauricio.vancine@gmail.com>
2021-08-07 00:20:37 -0300

    post install popso

--- post_install/post_install_popos_20_04.sh
+++ post_install/post_install_popos_20_04.sh
@@ -27,6 +27,11 @@ sudo apt install -y unrar
 # java
 sudo apt install -y default-jre default-jdk
 
+# git
+sudo add-apt-repository ppa:git-core/ppa && 
+sudo apt update &&
+sudo apt install -y git
+
 # sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - &&
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list &&
